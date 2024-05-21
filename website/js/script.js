bcc5c2a119246242dacdc257f5b4bdc345efce2a
Ken <69234258+MoriMomo@users.noreply.github.com>
2024-05-21 15:38:44 +0700

    .

--- website/js/script.js
+++ website/js/script.js
@@ -1,5 +1,8 @@
+
 // NAVBAR
 window.addEventListener("scroll", function () {
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
+
+Aos.init();
